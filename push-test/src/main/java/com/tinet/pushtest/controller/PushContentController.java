ba5bef90f6551bedc3ed95641bfccb51ba4231ea
dengjie1996 <1280075151@qq.com>
2022-12-02 11:18:04 +0800

    INIT

--- push-test/src/main/java/com/tinet/pushtest/controller/PushContentController.java
+++ push-test/src/main/java/com/tinet/pushtest/controller/PushContentController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * 类说明
  *
@@ -14,15 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PushContentController {
     public static String pushCpntent = "暂无数据";
+    SimpleDateFormat pdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static String pushTime = "暂无时间";
 
     @PostMapping("/pushContent")
     public String pushContent(@RequestBody String pushContent) {
         pushCpntent = pushContent;
+        pushTime = pdf.format(new Date());
         return "ok";
     }
 
     @GetMapping("/pushContent")
     public String getPushContent() {
-        return pushCpntent;
+        return "推送时间: \n" + pushTime + "\n" + "推送内容: \n" + pushCpntent;
     }
 }
